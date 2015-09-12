a46eab9451b2eb6629f972967aaf371e87617b73
Maik Hoepfel <m@maikhoepfel.de>
2015-09-12 13:23:05 +0200

    Login required for ProfileView
    I'm not sure whether we use it anywhere, but I thought it's better to
    protect the view. Not that it's technically possible to look up the user
    when there is None, but for consistency's sake...
    
    Also simplified get_object a bit.

--- scheduler/views.py
+++ scheduler/views.py
@@ -69,36 +69,18 @@ class HelpDesk(LoginRequiredMixin, TemplateView):
         return kwargs
 
 
-class ProfileView(UpdateView):
-    model = User
+class ProfileView(LoginRequiredMixin, UpdateView):
+    """
+    Allows a user to update their profile.
+
+    Maik isn't sure if this is linked to from anywhere. The template looks nasty.
+    """
     fields = ['first_name', 'last_name', 'email']
     template_name = "profile_edit.html"
-    success_url = reverse_lazy('profile_edit')
+    success_url = reverse_lazy('helpdesk')
 
     def get_object(self, queryset=None):
-        """
-        Returns the object the view is displaying.
-
-        By default this requires `self.queryset` and a `pk` or `slug` argument
-        in the URLconf, but subclasses can override this to return any object.
-        """
-        # Use a custom queryset if provided; this is required for subclasses
-        # like DateDetailView
-        if queryset is None:
-            queryset = self.get_queryset()
-
-        # Next, try looking up by primary key.
-        pk = self.request.user.pk
-        if pk is not None:
-            queryset = queryset.filter(pk=pk)
-
-        try:
-            # Get the single item from the filtered queryset
-            obj = queryset.get()
-        except queryset.model.DoesNotExist:
-            raise Http404(_("No %(verbose_name)s found matching the query") %
-                          {'verbose_name': queryset.model._meta.verbose_name})
-        return obj
+        return self.request.user
 
 
 class PlannerView(LoginRequiredMixin, FormView):
