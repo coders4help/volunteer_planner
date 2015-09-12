3c45ab1656a07365e33ee940192531bef359dcb1
Mei Li Triantafyllidi <meili.tr@gmail.com>
2015-09-12 23:45:29 +0200

    locally configurable language

--- man.py
+++ man.py
@@ -6,9 +6,12 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "volunteer_planner.settings.local")
     os.environ.setdefault("DATABASE_PW", "")
     os.environ.setdefault("DB_USER", "")
+    os.environ.setdefault("SECRET_KEY", "1")
+    os.environ.setdefault("LANGUAGE", "de")
+
+    # if you omit this values dummy email backend will be used
     os.environ.setdefault("EMAIL_ADDRESS", "")
     os.environ.setdefault("EMAIL_HOST_PASSWORD", "")
-    os.environ.setdefault("SECRET_KEY", "1")
 
     from django.core.management import execute_from_command_line
 
