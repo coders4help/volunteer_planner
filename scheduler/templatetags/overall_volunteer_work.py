c6f4e90c3c555eb56515f1526c34ec8c4d5216ce
Christoph Meißner <post@christophmeissner.de>
2015-09-16 13:15:13 +0200

    Remove TimePeriods model
    This removes indirection to simple datetime fields in Need model

--- scheduler/templatetags/overall_volunteer_work.py
+++ scheduler/templatetags/overall_volunteer_work.py
@@ -9,11 +9,11 @@ register = template.Library()
 @register.simple_tag
 def get_volunteer_hours():
     shifts = RegistrationProfile.objects.all() \
-        .filter(needs__time_period_from__date_time__lte=datetime.datetime.now())
+        .filter(needs__starting_time__lte=datetime.datetime.now())
     seconds = 0.0
     for shift in shifts:
         theshift = shift.needs.all()
         for single_shift in theshift:
-            deelta = single_shift.time_period_to.date_time - single_shift.time_period_from.date_time
+            deelta = single_shift.ending_time - single_shift.starting_time
             seconds += deelta.total_seconds()
     return int(((seconds / 60) / 60))
