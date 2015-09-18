d261ff69ae630e3ff6f6700d470f112a965b156b
Peter Palmreuther <pitpalme@yahoo.de>
2015-09-18 11:55:53 +0200

    New feature: email notification on shift change
    If shift times are changed (by more than 5 minutes) affected users are
    sent an email, stating old and new shift times.

--- scheduler/signals.py
+++ scheduler/signals.py
@@ -1,10 +1,15 @@
 # coding=utf-8
+import logging
 from django.core.mail import EmailMessage
-from django.db.models.signals import pre_delete
+
+from django.db.models.signals import pre_delete, post_save, pre_save
 from django.dispatch import receiver
 from django.template.loader import render_to_string
 
 from scheduler.models import Need
+
+logger = logging.getLogger(__name__)
+grace = 5*60  # 5 minutes in seconds
 
 
 @receiver(pre_delete, sender=Need)
@@ -30,3 +35,27 @@ def send_email_notifications(sender, instance, **kwargs):
                         from_email=from_email,
                         bcc=addresses)
     mail.send()
+
+
+@receiver(pre_save, sender=Need)
+def notify_users_shift_change(sender, instance, **kwargs):
+    shift = instance
+    old = Need.objects.get(pk=shift.pk)
+    diff_start = (old.starting_time - shift.starting_time).seconds
+    diff_end = (old.ending_time - shift.ending_time).seconds
+    if grace < diff_start or grace < diff_end:
+        subject = u'Schicht am {} wurde verändert'.format(shift.starting_time.strftime('%d.%m.%y'))
+
+        message = render_to_string('shift_modification_notification.html', dict(old=old, shift=shift))
+
+        from_email = "Volunteer-Planner.org <noreply@volunteer-planner.org>"
+
+        addresses = shift.registrationprofile_set.values_list('user__email', flat=True)
+        if 0 < len(addresses):
+            mail = EmailMessage(subject=subject, body=message, to=['support@volunteer-planner.org'], from_email=from_email,
+                                bcc=addresses)
+            logger.info(u'Shift %s at %s changed: (%s-%s -> %s->%s). Sending email notification to %d affected user(s).',
+                        shift.topic.title, shift.location.name,
+                        old.starting_time, old.ending_time, shift.starting_time, shift.ending_time,
+                        len(addresses))
+            mail.send()
