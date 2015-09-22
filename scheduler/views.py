0e977304e14728a2e1d6d1054eb075fdb9c6fd20
Maik Hoepfel <m@maikhoepfel.de>
2015-09-22 15:53:16 +0200

    Refactor volunteer hour calculation
    We had two ways implemented to calculate the total number of volunteer
    hours worked. One, a management command, required a cron job and relied
    on the ValueStore model. The other used a SQL view, which needed manual
    setting up.
    
    I'd argue Django offers us everything we need to inexpensively calculate
    the total number of hours. I've moved two existing template tags into
    the new stats app, and reworked the one that calculates the total hours.
    
    I've also added a default ordering to Needs, and sprinkled a bit of
    caching.

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,25 +1,17 @@
 # coding: utf-8
 
-import json
-import datetime
 import logging
 
 from django.core.urlresolvers import reverse
 from django.contrib import messages
-from django.db import ProgrammingError, OperationalError
-from django.http.response import HttpResponseRedirect, JsonResponse
-from django.shortcuts import render, get_object_or_404
+from django.http.response import HttpResponseRedirect
 from django.db.models import Count
 from django.views.generic import TemplateView, FormView
 
-from django.contrib.auth.decorators import login_required, permission_required
-
 from django.utils.translation import ugettext_lazy as _
 
-from scheduler.models import Location, Need, WorkDone
+from scheduler.models import Location, Need
 from notifications.models import Notification
-from registration.models import RegistrationProfile
-from stats.models import ValueStore
 from .forms import RegisterForNeedForm
 from volunteer_planner.utils import LoginRequiredMixin
 
@@ -40,16 +32,6 @@ class HomeView(TemplateView):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['locations'] = Location.objects.all()
         context['notifications'] = Notification.objects.all()
-        try:
-            work_done = WorkDone.objects.get(pk=1)
-        except (WorkDone.DoesNotExist, ProgrammingError, OperationalError):
-            # In case the unmanaged model isn't created correctly or a developer is e.g. using
-            # SQLite instead of MySQL, we don't want to fail loud and hard. The work done is
-            # auxiliary information and we just log a warning.
-            logger.warning(u"Work done view couldn't be accessed. Is the SQL view created?")
-            work_done = ""
-        finally:
-            context['working_hours'] = work_done
         return context
 
 
