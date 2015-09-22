071c8c5feeadf2572a9afc8bd99bfe5ce066e99a
Fabian Lindenberg <fabian.lindenberg@gmail.com>
2015-09-22 19:11:02 +0200

    introduced first test case for model Topic

--- tests/scheduler/test_models.py
+++ tests/scheduler/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 import datetime
 
-from tests.factories import NeedFactory
+from tests.factories import NeedFactory 
 
 
 def create_need(start_hour, end_hour):
@@ -37,3 +37,28 @@ class NeedTestCase(TestCase):
     def test_conflict_0h_grace(self):
         need = create_need(11, 14)
         assert need.get_conflicting_needs(self.needs, grace=datetime.timedelta(hours=0))
+
+class LocationTestCase(TestCase):
+
+    def setUp(self):
+        now = datetime.datetime.now()
+        yesterday_start = now - datetime.timedelta(1)
+        yesterday_end = yesterday_start+datetime.timedelta(hours=1)
+        tomorrow_start = now + datetime.timedelta(1)
+        tomorrow_end = tomorrow_start+datetime.timedelta(hours=1)
+
+        needs = [NeedFactory.create(starting_time=yesterday_start, ending_time=yesterday_end),
+                    NeedFactory.create(starting_time=tomorrow_start, ending_time=tomorrow_end)]
+
+        self.locations = []
+        for need in needs:
+            self.locations.append(need.location)
+
+    def test_get_days_with_needs_at_locations_end_later_than_now(self):
+        """
+            checks that get_days_with_needs() returns only dates later than datetime.now()
+        """
+        for location in self.locations:
+            for day in location.get_days_with_needs():
+                assert isinstance(day[0], datetime.datetime)
+                assert day[0] > datetime.datetime.now()
