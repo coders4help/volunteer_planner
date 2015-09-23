927cd133aa413a5774a2059d0f74ec147e87a040
Fabian Lindenberg <fabian.lindenberg@gmail.com>
2015-09-23 16:34:09 +0200

    added test case for empty submit

--- tests/registration/test_registration.py
+++ tests/registration/test_registration.py
@@ -4,13 +4,20 @@ from django.core.urlresolvers import reverse
 
 class RegistrationTestCase(TestCase):
 
-    def test_displays_empty_form(self):
+    def test_get_displays_empty_form(self):
         registration_url = reverse('registation')  # TODO: fix typo in url name in urls.py
 
         response = self.client.get(registration_url)
-        form = response.context['form']
+        assert response.status_code == 200
 
+        form = response.context['form']
         assert form is not None
 
         initial_data = form.initial
         assert len(initial_data) == 0
+
+    def test_submit_empty_form(self):
+        registration_url = reverse('registation')  # TODO: fix typo in url name in urls.py
+        response = self.client.post(registration_url, {})  # shall not raise an exception
+
+        self.assertContains(response, 'Dieses Feld ist zwingend erforderlich.')
