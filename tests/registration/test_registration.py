092013e9b2e2d3ab9036996c875393285b2f1fa1
Fabian Lindenberg <fabian.lindenberg@gmail.com>
2015-09-23 17:15:54 +0200

    introduced test case for valid registration

--- tests/registration/test_registration.py
+++ tests/registration/test_registration.py
@@ -2,10 +2,14 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 
 
+from registration.models import RegistrationProfile
+
+
 class RegistrationTestCase(TestCase):
 
     def test_get_displays_empty_form(self):
-        registration_url = reverse('registation')  # TODO: fix typo in url name in urls.py
+        # TODO: fix typo in url name in urls.py
+        registration_url = reverse('registation')
 
         response = self.client.get(registration_url)
         assert response.status_code == 200
@@ -17,7 +21,32 @@ class RegistrationTestCase(TestCase):
         assert len(initial_data) == 0
 
     def test_submit_empty_form(self):
-        registration_url = reverse('registation')  # TODO: fix typo in url name in urls.py
-        response = self.client.post(registration_url, {})  # shall not raise an exception
+        # TODO: fix typo in url name in urls.py
+        registration_url = reverse('registation')
+
+        # shall not raise an exception
+        response = self.client.post(registration_url, {})
+
+        form = response.context['form']
+        assert form is not None, "We expect the form to be displayed again if the submission failed"
 
         self.assertContains(response, 'Dieses Feld ist zwingend erforderlich.')
+
+    def test_submit_valid_form(self):
+        # TODO: fix typo in url name in urls.py
+        registration_url = reverse('registation')
+
+        response = self.client.post(registration_url,
+                                    {'username': 'somename',
+                                     'email': 'myaddress@example.com',
+                                     'password1': 'somepassword',
+                                     'password2': 'somepassword'},
+                                    follow=True)
+
+        assert response.status_code == 200
+        self.assertContains(
+            response, 'Eine Aktivierungsmail wurde Dir soeben zugesendet.')
+
+        new_user = RegistrationProfile.objects.first()
+        assert new_user is not None
+        assert new_user.user.username == "somename"
