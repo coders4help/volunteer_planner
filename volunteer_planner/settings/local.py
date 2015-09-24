67d56e5777be7df485df9ac939ad7d59d9caf9cc
Christoph Meißner <post@christophmeissner.de>
2015-09-24 18:49:13 +0200

    Moved sqlite3 db file out of repository
    ... to avoid getting deleted on git clean. Please move your local sqlite accordingly (one directory up).

--- volunteer_planner/settings/local.py
+++ volunteer_planner/settings/local.py
@@ -6,7 +6,7 @@ INTERNAL_IPS = ('127.0.0.1',)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(SITE_ROOT, 'db.sqlite3'),
+        'NAME': os.path.join(SITE_ROOT, '..', 'db.sqlite3'),
     }
 }
 
