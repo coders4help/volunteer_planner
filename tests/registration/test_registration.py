786d3a87d06d4d4b97224a117b95e46de7d8b580
Fabian Lindenberg <fabian.lindenberg@gmail.com>
2015-09-24 14:07:23 +0200

    refactored tests

--- tests/registration/test_registration.py
+++ tests/registration/test_registration.py
@@ -7,16 +7,17 @@ from registration.models import RegistrationProfile
 
 class RegistrationTestCase(TestCase):
 
-    valid_user_data = {'username': 'somename',
-                       'email': 'somename@example.de',
-                       'password1': 'somepassword',
-                       'password2': 'somepassword'}
+    def setUp(self):
+        # TODO: fix typo in url name in urls.py
+        self.registration_url = reverse('registation')
+
+        self.valid_user_data = {'username': 'somename',
+                                'email': 'somename@example.de',
+                                'password1': 'somepassword',
+                                'password2': 'somepassword'}
 
     def test_get_displays_empty_form(self):
-        # TODO: fix typo in url name in urls.py
-        registration_url = reverse('registation')
-
-        response = self.client.get(registration_url)
+        response = self.client.get(self.registration_url)
         assert response.status_code == 200
         self.assertTemplateUsed(response, 'registration_form.html')
 
@@ -27,11 +28,8 @@ class RegistrationTestCase(TestCase):
         assert len(initial_data) == 0
 
     def test_submit_empty_form(self):
-        # TODO: fix typo in url name in urls.py
-        registration_url = reverse('registation')
-
         # shall not raise an exception
-        response = self.client.post(registration_url, {})
+        response = self.client.post(self.registration_url, {})
 
         form = response.context['form']
         assert form is not None, 'We expect the form to be displayed again if the submission failed'
@@ -45,10 +43,7 @@ class RegistrationTestCase(TestCase):
         assert RegistrationProfile.objects.count() == 0
 
     def test_submit_valid_form(self):
-        # TODO: fix typo in url name in urls.py
-        registration_url = reverse('registation')
-
-        response = self.client.post(registration_url,
+        response = self.client.post(self.registration_url,
                                     self.valid_user_data,
                                     follow=True)
 
@@ -64,14 +59,11 @@ class RegistrationTestCase(TestCase):
         assert new_user.user.username == "somename"
 
     def test_username_exists_already(self):
-        # TODO: fix typo in url name in urls.py
-        registration_url = reverse('registation')
-
         # register first user
-        self.client.post(registration_url, self.valid_user_data)
+        self.client.post(self.registration_url, self.valid_user_data)
 
         # try to register user again (=same username)
-        response = self.client.post(registration_url,
+        response = self.client.post(self.registration_url,
                                     self.valid_user_data)
 
         form = response.context['form']
@@ -86,15 +78,12 @@ class RegistrationTestCase(TestCase):
         assert RegistrationProfile.objects.count() == 1
 
     def test_passwords_dont_match(self):
-        # TODO: fix typo in url name in urls.py
-        registration_url = reverse('registation')
-
         user_data = {'username': 'somename',
                      'email': 'somename@example.de',
                      'password1': 'somepassword',
                      'password2': 'differentpassword'}
 
-        response = self.client.post(registration_url, user_data)
+        response = self.client.post(self.registration_url, user_data)
 
         form = response.context['form']
         assert form is not None, 'We expect the form to be displayed again if the submission failed'
@@ -113,15 +102,12 @@ class RegistrationTestCase(TestCase):
         """
         helper method for the next couple of tests checking invalid usernames
         """
-        # TODO: fix typo in url name in urls.py
-        registration_url = reverse('registation')
-
         user_data = {'username': invalid_username,
                      'email': 'somename@example.de',
                      'password1': 'somepassword',
                      'password2': 'somepassword'}
 
-        response = self.client.post(registration_url, user_data)
+        response = self.client.post(self.registration_url, user_data)
 
         form = response.context['form']
         assert form is not None, 'We expect the form to be displayed again if the submission failed'
@@ -144,19 +130,13 @@ class RegistrationTestCase(TestCase):
         self.try_invalid_username(unicode('somename\xc3', errors='replace'))
 
     def test_registered_user_is_inactive(self):
-        # TODO: fix typo in url name in urls.py
-        registration_url = reverse('registation')
-
-        self.client.post(registration_url, self.valid_user_data)
+        self.client.post(self.registration_url, self.valid_user_data)
 
         new_user = RegistrationProfile.objects.first()
         assert not new_user.user.is_active
 
     def test_activate_registered_user(self):
-        # TODO: fix typo in url name in urls.py
-        registration_url = reverse('registation')
-
-        self.client.post(registration_url, self.valid_user_data)
+        self.client.post(self.registration_url, self.valid_user_data)
 
         new_user = RegistrationProfile.objects.first()
 
@@ -165,7 +145,9 @@ class RegistrationTestCase(TestCase):
         activation_url = reverse('user_activate')
         activation_key = new_user.activation_key
 
-        response = self.client.get(activation_url, data={'activation_key': activation_key}, follow=True)
+        response = self.client.get(activation_url,
+                                   data={'activation_key': activation_key},
+                                   follow=True)
 
         activation_complete_url = reverse('registration_activation_complete')
 
