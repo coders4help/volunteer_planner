a2e9ea4a99a92a734fd226e126bcb32463122cf4
Christoph Meißner <post@christophmeissner.de>
2015-09-26 00:31:04 +0200

    Clean up home view template tag usage

--- non_logged_in_area/templatetags/volunteer_numbers.py
+++ non_logged_in_area/templatetags/volunteer_numbers.py
@@ -36,3 +36,11 @@ def get_volunteer_hours():
     hours = int(delta.total_seconds() / 3600)
     return hours
 
+
+@register.assignment_tag
+def get_volunteer_stats():
+    return {
+        'volunteer_count': get_volunteer_number(),
+        'facility_count': get_facility_count(),
+        'volunteer_hours': get_volunteer_hours(),
+    }
