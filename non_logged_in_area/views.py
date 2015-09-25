f6f84b75d85d9091774458363511307b23389905
Christoph Meißner <post@christophmeissner.de>
2015-09-25 17:30:21 +0200

    Add places to home view

--- non_logged_in_area/views.py
+++ non_logged_in_area/views.py
@@ -1,13 +1,15 @@
+import logging
+
 from django.db import ProgrammingError, OperationalError
 from django.db.models.aggregates import Count
 from django.http.response import HttpResponseRedirect
 from django.views.generic.base import TemplateView
+
 from django.core.urlresolvers import reverse
-import logging
 
 from notifications.models import Notification
 from scheduler.models import WorkDone
-from places.models import Region
+from places.models import Region, Municipality
 
 logger = logging.getLogger(__name__)
 
@@ -23,11 +25,16 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['regions'] = Region.objects.annotate(locations_count=Count('areas__municipalities__locations')).exclude(locations_count=0).prefetch_related('areas', 'areas__region').all()
+        context['regions'] = Region.objects.annotate(
+            locations_count=Count('areas__municipalities__locations')).exclude(
+            locations_count=0).prefetch_related('areas', 'areas__region').all()
+        context['places'] = Municipality.objects.annotate(
+            locations_count=Count('locations')).exclude(
+            locations_count=0).select_related('area', 'area__region').all()
         for i in context['regions']:
-            location=i
+            location = i
             pass
-        #context['regions'] = Region.objects.all().prefetch_related('areas__municipalities__locations')
+        # context['regions'] = Region.objects.all().prefetch_related('areas__municipalities__locations')
         context['notifications'] = Notification.objects.all()
         try:
             work_done = WorkDone.objects.get(pk=1)
@@ -35,9 +42,9 @@ class HomeView(TemplateView):
             # In case the unmanaged model isn't created correctly or a developer is e.g. using
             # SQLite instead of MySQL, we don't want to fail loud and hard. The work done is
             # auxiliary information and we just log a warning.
-            logger.warning(u"Work done view couldn't be accessed. Is the SQL view created?")
+            logger.warning(
+                u"Work done view couldn't be accessed. Is the SQL view created?")
             work_done = ""
         finally:
             context['working_hours'] = work_done
         return context
-
