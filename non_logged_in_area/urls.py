ff9261d0561fb5f4c78b40daeeadc5bdd2d4f7df
Dorian <cantzen@googlemail.com>
2015-09-27 11:48:14 +0200

    Add shelters need help page for relaunch

--- non_logged_in_area/urls.py
+++ non_logged_in_area/urls.py
@@ -4,4 +4,5 @@ from .views import HomeView
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(template_name="home.html"), name="home"),
+    url(r'^shelters-need-help/$', HomeView.as_view(template_name="shelters_need_help.html"), name="home"),
 ]
