a22f5ac814c51f3680be1175e0083e7cc8cee638
Christoph Meißner <post@christophmeissner.de>
2015-09-30 18:32:13 +0200

    Remove ValueStats model

--- stats/models.py
+++ stats/models.py
@@ -1,17 +1,1 @@
-"""
-This app is mainly for storing values of saving calculations to display
-on the website.
-
-As far as Maik can tell, it's unused at the moment.
-"""
-
-from django.db import models
-
-
-class ValueStore(models.Model):
-
-    name = models.CharField(max_length=255)
-    value = models.CharField(max_length=255)
-
-    def __unicode__(self):
-        return self.name
+# coding=utf-8
