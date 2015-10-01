9297f2ffe0750ce4a40a35666ce9abb4bbfa487a
Christoph Meißner <post@christophmeissner.de>
2015-10-01 13:17:50 +0200

    Add signal hook for Account creation on completed registration

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,8 @@
 from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from registration.signals import user_activated
+from django.dispatch import receiver
 
 
 class UserAccount(models.Model):
@@ -12,3 +14,10 @@ class UserAccount(models.Model):
     class Meta:
         verbose_name = _('user account')
         verbose_name_plural = _('user accounts')
+
+@receiver(user_activated)
+def registration_completed(sender, user, request, **kwargs):
+    account, created = UserAccount.objects.get_or_create(user=user)
+    print account, created
+
+
