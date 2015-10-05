83406ac84bbb46967067a553e5f5526fb288dabb
Christoph Meißner <post@christophmeissner.de>
2015-10-05 14:14:11 +0200

    Add admin for UserAccount

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
+# Register your models here.
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserAccount
+
+
+@admin.register(UserAccount)
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = (u'id', 'user')
+    raw_id_fields = ('user',)
