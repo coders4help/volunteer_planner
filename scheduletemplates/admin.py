e587bb35997a7bbfbd2b9e70f7456af61c938a13
Christoph Meißner <post@christophmeissner.de>
2015-10-09 12:28:43 +0200

    wip

--- scheduletemplates/admin.py
+++ scheduletemplates/admin.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
+from datetime import timedelta
+
 from django.conf.urls import url
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.core import urlresolvers
 from django.db.models import Min, Count, Sum
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
+from django.templatetags.l10n import localize
+from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
+import dateutil.parser as date_parser
 
 from .models import ScheduleTemplate, ShiftTemplate
 
@@ -41,30 +48,48 @@ class ScheduleTemplateAdmin(admin.ModelAdmin):
             context.update({
                 "opts": self.model._meta,
                 "schedule_template": schedule_template,
-                "shift_templates": shift_templates
+                "shift_templates": shift_templates,
+                "preselected_date": timezone.now().date() + timedelta(days=1)
             })
 
             return TemplateResponse(request,
                                     "admin/scheduletemplates/apply_template.html",
                                     context)
         elif request.method == 'POST':
+
             id_list = request.POST.getlist('selected_shift_templates', [])
-            selected_date = request.POST.get('selected_date')
+            selected_date_str = request.POST.get('selected_date', None)
+
+            if selected_date_str:
+                selected_date = date_parser.parse(selected_date_str)
+
             if id_list:
-                selected_shifts = shift_templates.filter(id__in=id_list)
+                selected_shift_templates = shift_templates.filter(id__in=id_list)
 
-            #print selected_shifts
-            #print selected_date
+            if request.POST.get('apply', None):
 
-            context.update({
-                "opts": self.model._meta,
-                "schedule_template": schedule_template,
-                "selected_shifts": selected_shifts,
-                "selected_date": selected_date
-            })
-            return TemplateResponse(request,
-                                    "admin/scheduletemplates/apply_template_confirm.html",
-                                    context)
+                context.update({
+                    "opts": self.model._meta,
+                    "schedule_template": schedule_template,
+                    "selected_shifts": selected_shift_templates,
+                    "selected_date": selected_date
+                })
+                return TemplateResponse(request,
+                                        "admin/scheduletemplates/apply_template_confirm.html",
+                                        context)
+            elif request.POST.get('confirm', None):
+                messages.success(request, _(
+                    u'{} shifts were added to {}').format(len(id_list),
+                                                          localize(
+                                                              selected_date)))
+                for shift_templates in selected_shift_templates:
+                    
+
+                return HttpResponseRedirect(
+                    urlresolvers.reverse(
+                        'admin:scheduletemplates_scheduletemplate_change',
+                        args=(pk,), current_app='custom')
+                )
 
     def get_urls(self):
         urls = super(ScheduleTemplateAdmin, self).get_urls()
@@ -156,6 +181,17 @@ class ShiftTemplateAdmin(admin.ModelAdmin):
             if self.parent_model.facility:
                 qs = qs.filter(facility=self.parent_model.facility)
 
+        return qs
+
+    def get_queryset(self, request):
+        qs = super(ShiftTemplateAdmin, self).get_queryset(request)
+        return self.model.objects.select_related('schedule_template',
+                                                 'schedule_template__facility',
+                                                 'workplace',
+                                                 'workplace__facility',
+                                                 'task',
+                                                 'task__facility'
+                                                 )
         return qs
 
 
