6c9be0142e545573fb5a20f3990c1fd68a82becd
Maik Hoepfel <m@maikhoepfel.de>
2015-10-09 16:47:03 +0200

    TLC for new blueprinter
    This commit switches to using a form for selecting the date. That gives
    us a decent enough date picker for free. Messing with Javascript
    polyfills turned out to be too painful.
    
    I also added a few comments here and there to make the process a bit
    more readable.

--- scheduletemplates/admin.py
+++ scheduletemplates/admin.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 from datetime import timedelta, datetime
 
+from django import forms
 from django.conf.urls import url
 from django.contrib import admin, messages
-from django.core import urlresolvers
 from django.db.models import Min, Count, Sum
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+from django.forms.extras import SelectDateWidget
+from django.shortcuts import get_object_or_404, redirect
 from django.template.response import TemplateResponse
 from django.templatetags.l10n import localize
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
-import dateutil.parser as date_parser
 
 from .models import ScheduleTemplate, ShiftTemplate
 from scheduler.models import Shift
@@ -34,74 +33,92 @@ class ShiftTemplateInline(admin.TabularInline):
         return qs
 
 
+class ApplyTemplateForm(forms.Form):
+    """
+    Form that lets one select a date. We mostly use it because it lets us use
+    Django's decent enough date selector.
+
+    TODO: Also select shifts via the form instead of inspecting raw POST data.
+          https://docs.djangoproject.com/en/1.8/ref/forms/fields/#modelmultiplechoicefield
+    """
+    apply_for_date = forms.DateField(widget=SelectDateWidget)
+
+
 class ScheduleTemplateAdmin(admin.ModelAdmin):
-    inlines = [
-        ShiftTemplateInline
-    ]
+    inlines = [ShiftTemplateInline]
 
     def apply_schedule_template(self, request, pk):
+        """
+        Juicy function that lets one create a schedule template, and
+        then apply the template on one date to create individual shifts.
+
+        Has three phases:
+        1. GET: Allow selecting date and shifts
+        2. POST: Displays a preview of what will be done
+        3. POST: Actually apply the template.
+        """
         schedule_template = get_object_or_404(self.model, pk=pk)
         shift_templates = schedule_template.shift_templates.all()
 
         context = dict(self.admin_site.each_context(request))
-        # TODO: use a form class
+        context["opts"] = self.model._meta  # Needed for admin template breadcrumbs
+
+        # Phase 1
         if request.method == 'GET':
+            # Empty form, prepopulated with tomorrow's date
+            form = ApplyTemplateForm(
+                initial={'apply_for_date': timezone.now().date() + timedelta(days=1)})
             context.update({
-                "opts": self.model._meta,
                 "schedule_template": schedule_template,
                 "shift_templates": shift_templates,
-                "preselected_date": timezone.now().date() + timedelta(days=1)
+                "apply_form": form,
             })
 
-            return TemplateResponse(request,
-                                    "admin/scheduletemplates/apply_template.html",
-                                    context)
+            return TemplateResponse(
+                request, "admin/scheduletemplates/apply_template.html", context)
+
+        # Phase 2 and 3
         elif request.method == 'POST':
+            # Verify the form data.
+            form = ApplyTemplateForm(request.POST)
+            if form.is_valid():
+                apply_date = form.cleaned_data['apply_for_date']
+            else:
+                # Shouldn't happen, but let's make sure we don't proceed with
+                # applying shifts.
+                raise ValueError("Invalid date format")
 
+            # Get selected shifts.
+            # TODO: This should be done with a ModelMultipleChoiceField on the form.
             id_list = request.POST.getlist('selected_shift_templates', [])
-            selected_date_str = request.POST.get('selected_date', None)
+            selected_shift_templates = shift_templates.filter(id__in=id_list)
 
-            if selected_date_str:
-                selected_date = date_parser.parse(selected_date_str).date()
-
-            if id_list:
-                selected_shift_templates = shift_templates.filter(
-                    id__in=id_list)
-
-            if request.POST.get('apply', None):
-
+            # Phase 2: display a preview
+            if request.POST.get('apply'):
                 context.update({
-                    "opts": self.model._meta,
                     "schedule_template": schedule_template,
                     "selected_shifts": selected_shift_templates,
-                    "selected_date": selected_date
+                    "selected_date": apply_date,
+                    "apply_form": form,  # Needed because we need to POST the data again
                 })
-                return TemplateResponse(request,
-                                        "admin/scheduletemplates/apply_template_confirm.html",
-                                        context)
-            elif request.POST.get('confirm', None):
-                messages.success(request, _(
-                    u'{} shifts were added to {}').format(len(id_list),
-                                                          localize(
-                                                              selected_date)))
+                return TemplateResponse(
+                    request, "admin/scheduletemplates/apply_template_confirm.html", context)
 
+            # Phase 3: Create shifts
+            elif request.POST.get('confirm'):
                 for template in selected_shift_templates:
+                    starting_time = datetime.combine(apply_date, template.starting_time)
+                    Shift.objects.create(
+                        starting_time=starting_time,
+                        ending_time=starting_time + template.duration,
+                        task=template.task,
+                        workplace=template.workplace,
+                        slots=template.slots)
 
-                    shift = Shift()
-                    shift.starting_time = datetime.combine(selected_date,
-                                                           template.starting_time)
-                    shift.ending_time = shift.starting_time + template.duration
-                    shift.task = template.task
-                    shift.workplace = template.workplace
-                    shift.slots = template.slots
-                    shift.facility = template.schedule_template.facility
-                    shift.save()
+                messages.success(request, _(
+                    u'{} shifts were added to {}').format(len(id_list), localize(apply_date)))
 
-                return HttpResponseRedirect(
-                    urlresolvers.reverse(
-                        'admin:scheduletemplates_scheduletemplate_change',
-                        args=(pk,), current_app='custom')
-                )
+                return redirect('admin:scheduletemplates_scheduletemplate_change', pk)
 
     def get_urls(self):
         urls = super(ScheduleTemplateAdmin, self).get_urls()
