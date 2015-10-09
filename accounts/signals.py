15e86c0da16747c05c668a771e3e0b8f57eead63
Christoph Meißner <post@christophmeissner.de>
2015-10-10 00:43:52 +0200

    Remove print statement

--- accounts/signals.py
+++ accounts/signals.py
@@ -8,5 +8,4 @@ from .models import UserAccount
 
 @receiver(user_activated)
 def registration_completed(sender, user, request, **kwargs):
-    account, created = UserAccount.objects.get_or_create(user=user)
-    print account, created
+    UserAccount.objects.get_or_create(user=user)
