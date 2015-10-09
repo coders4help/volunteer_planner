b800fda83cd8d1659c5cbd0a277bf3710f503d36
Christoph Meißner <post@christophmeissner.de>
2015-10-10 00:45:19 +0200

    Remove EnrolmentManager class
    EnrolmentManager was not used (I think it was intended to replace the ShiftHelperManager)

--- scheduler/managers.py
+++ scheduler/managers.py
@@ -9,25 +9,6 @@ from places import models as place_models
 from organizations import models as organization_models
 
 
-class EnrolmentManager(models.Manager):
-    def conflicting(self, shift, user_account=None, grace=timedelta(hours=1)):
-
-        grace = grace or timedelta(0)
-        graced_start = shift.starting_time + grace
-        graced_end = shift.ending_time - grace
-
-        query_set = self.get_queryset().select_related('shift', 'user_account')
-
-        if user_account:
-            query_set = query_set.filter(user=user_account)
-
-        query_set = query_set.exclude(shift__start_time__lt=graced_start,
-                                      shift__end_time__lte=graced_start)
-        query_set = query_set.exclude(shift__start_time__gte=graced_end,
-                                      shift__end_time__gte=graced_end)
-        return query_set
-
-
 class ShiftManager(models.Manager):
     def at_facility(self, facility):
         return self.get_queryset().filter(facility=facility)
