6d6cc6fb40246795e1304ef19e21aa721d415ddc
Christoph Meißner <post@christophmeissner.de>
2015-10-22 14:43:19 +0200

    Add migration

---
+++ organizations/migrations/0006_auto_20151022_1438.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('organizations', '0005_auto_20151021_1153'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='facilitymembership',
+            name='role',
+            field=models.PositiveIntegerField(default=2, verbose_name='role', choices=[(0, 'Admin'), (1, 'Manager'), (2, 'Member')]),
+        ),
+        migrations.AlterField(
+            model_name='organizationmembership',
+            name='role',
+            field=models.PositiveIntegerField(default=2, verbose_name='role', choices=[(0, 'Admin'), (1, 'Manager'), (2, 'Member')]),
+        ),
+    ]
