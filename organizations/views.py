9d43c15ee609bcb55ac775f60987bf7681b0b8f8
Jan Mewes <jan.mewes@experimental-software.com>
2015-10-25 12:54:38 +0100

    Filter shifts by organization

--- organizations/views.py
+++ organizations/views.py
@@ -3,21 +3,24 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import UpdateView
 from django.core.urlresolvers import reverse_lazy
+from datetime import date
 
 from volunteer_planner.utils import LoginRequiredMixin
+from organizations.models import OrganizationMembership
 
 from scheduler.models import Shift
+from organizations.admin import filter_queryset_by_membership
 
 
 @login_required()
 def shift_management(request):
+    current_user = request.user
     
     # Get all shifts scheduled in future for the organization of the current user
-    all_shifts = Shift.objects.all()
-    # TODO apply filter for only future shifts
-    
-    context = {'shifts': all_shifts}
-    
+    open_shifts = Shift.open_shifts.all()
+    open_shifts = filter_queryset_by_membership(open_shifts, current_user)
+
+    context = {'shifts': open_shifts}
     return render(request, 'organizations/shift_manage.html', context)
 
 @login_required()
@@ -40,3 +43,4 @@ def shift_delete(request, shift_id):
 
 	# TODO: implement delete and redirect
     return render(request, 'organizations/shift_edit.html', context)
+
