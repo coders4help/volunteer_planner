dc551c1aa2e669aaf183c7b590aae06cc7bba4d7
Christoph Meißner <post@christophmeissner.de>
2015-10-27 13:35:57 +0100

    Add 'request_site' template tag
    Gets the current requests' site (domain) in templates.
    
    Example usage in templates (request is supposed to be a variable containing the current request):
    
    {% load site %}
    {% request_site request as site %}
    
    {{ site.domain }}

---
+++ common/templatetags/site.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+# coding: utf-8
+
+from django import template
+
+from django.contrib.sites.shortcuts import get_current_site
+
+register = template.Library()
+
+
+@register.assignment_tag
+def request_site(request):
+    return get_current_site(request_site)
