52faaf6d4ecb6a5bdd4907277c26a4fe51f26e6e
Christoph Meißner <post@christophmeissner.de>
2015-10-29 19:38:49 +0100

    Quickfix email notifications
    Notifications are now only sent, when shift is not over yet and there is actually sombody enrolled.

--- scheduler/signals.py
+++ scheduler/signals.py
@@ -1,15 +1,17 @@
 # coding=utf-8
 import logging
-from django.core.mail import EmailMessage
+from datetime import datetime
 
-from django.db.models.signals import pre_delete, post_save, pre_save
+from django.template.defaultfilters import time as date_filter
+from django.core.mail import EmailMessage
+from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 from django.template.loader import render_to_string
 
 from scheduler.models import Shift
 
 logger = logging.getLogger(__name__)
-grace = 5*60  # 5 minutes in seconds
+grace = 5 * 60  # 5 minutes in seconds
 
 
 @receiver(pre_delete, sender=Shift)
@@ -21,43 +23,55 @@ def send_email_notifications(sender, instance, **kwargs):
     add some error handling, some sane max recipient handling, tests, etc.
     """
     shift = instance
-    subject = u'Schicht am {} wurde abgesagt'.format(shift.starting_time.strftime('%d.%m.%y'))
+    if shift.ending_time >= datetime.now():
+        subject = u'Schicht am {} wurde abgesagt'.format(
+            shift.starting_time.strftime('%d.%m.%y'))
 
-    message = render_to_string('shift_cancellation_notification.html', dict(shift=shift))
+        message = render_to_string('shift_cancellation_notification.html',
+                                   dict(shift=shift))
 
-    from_email = "Volunteer-Planner.org <noreply@volunteer-planner.org>"
+        from_email = "Volunteer-Planner.org <noreply@volunteer-planner.org>"
 
-    addresses = shift.helpers.values_list('user__email', flat=True)
+        addresses = shift.helpers.values_list('user__email', flat=True)
 
-    mail = EmailMessage(subject=subject, body=message,
-                        to=['support@volunteer-planner.org'],
-                        from_email=from_email,
-                        bcc=addresses)
-    mail.send()
+        if addresses:
+            mail = EmailMessage(subject=subject, body=message,
+                                to=['support@volunteer-planner.org'],
+                                from_email=from_email,
+                                bcc=addresses)
+            mail.send()
 
 
 @receiver(pre_save, sender=Shift)
 def notify_users_shift_change(sender, instance, **kwargs):
     shift = instance
-    old = Shift.objects.filter(pk=shift.pk)
-    # Test whether this is modification or creation, to avoid DoesNotExist exception
-    if shift.pk and old.exists():
-        old_shift = old.get()
-        diff_start = (old_shift.starting_time - shift.starting_time).seconds
-        diff_end = (old_shift.ending_time - shift.ending_time).seconds
-        if grace < diff_start or grace < diff_end:
-            subject = u'Schicht am {} wurde verändert'.format(shift.starting_time.strftime('%d.%m.%y'))
 
-            message = render_to_string('shift_modification_notification.html', dict(old=old_shift, shift=shift))
+    if shift.pk:
+        old_shift = Shift.objects.get(pk=shift.pk)
+
+        # Test whether this is modification or creation, to avoid DoesNotExist exception
+        if old_shift.ending_time >= datetime.now():
+
+            subject = u'Schicht wurde verändert: {task} am {date}'.format(
+                task=old_shift.task.name,
+                date=date_filter(old_shift.starting_time))
+
+            message = render_to_string('shift_modification_notification.html',
+                                       dict(old=old_shift, shift=shift))
 
             from_email = "Volunteer-Planner.org <noreply@volunteer-planner.org>"
 
             addresses = shift.helpers.values_list('user__email', flat=True)
-            if 0 < len(addresses):
-                mail = EmailMessage(subject=subject, body=message, to=['support@volunteer-planner.org'], from_email=from_email,
+            if addresses:
+                mail = EmailMessage(subject=subject,
+                                    body=message,
+                                    to=['support@volunteer-planner.org'],
+                                    from_email=from_email,
                                     bcc=addresses)
-                logger.info(u'Shift %s at %s changed: (%s-%s -> %s->%s). Sending email notification to %d affected user(s).',
-                            shift.task.name, shift.facility.name,
-                            old_shift.starting_time, old_shift.ending_time, shift.starting_time, shift.ending_time,
-                            len(addresses))
+                logger.info(
+                    u'Shift %s at %s changed: (%s-%s -> %s->%s). Sending email notification to %d affected user(s).',
+                    shift.task.name, shift.facility.name,
+                    old_shift.starting_time, old_shift.ending_time,
+                    shift.starting_time, shift.ending_time,
+                    len(addresses))
                 mail.send()
