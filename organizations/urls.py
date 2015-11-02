b5dbfc1a740c2b9233198912e9fb1d7d15dc9873
Dorian Cantzen <cantzen@googlemail.com>
2015-11-02 23:25:33 +0100

    Add approval templates without functionality

--- organizations/urls.py
+++ organizations/urls.py
@@ -1,12 +1,15 @@
 # coding: utf-8
 
 from django.conf.urls import url
+from django.views.generic.base import TemplateView
 
 from .views import OrganizationView, FacilityView
 
 urlpatterns = [
-
+    url(r'^approvals/$', TemplateView.as_view(template_name="approvals.html"), name='approvals'),
     url(r'^(?P<slug>[^/]+)/?$', OrganizationView.as_view(), name='organization'),
     url(r'^(?P<organization__slug>[^/]+)/(?P<slug>[^/]+)/?$', FacilityView.as_view(), name='facility'),
+
+
 
 ]
