59dabe9188373c7afdf883649069c1ddaf4b098d
Christoph Meißner <post@christophmeissner.de>
2015-11-08 17:26:15 +0100

    Fix volunteer count underflow
    Fix for "Don't allow negative number of remaining slots for shift #253"

--- scheduler/forms.py
+++ scheduler/forms.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 
 from django import forms
+from django.db.models import Count
 
 from scheduler.models import Shift
 
 
 class RegisterForShiftForm(forms.Form):
     leave_shift = forms.ModelChoiceField(queryset=Shift.objects, required=False)
-    join_shift = forms.ModelChoiceField(queryset=Shift.objects, required=False)
+    join_shift = forms.ModelChoiceField(queryset=Shift.objects.annotate(volunteer_count=Count('helpers')), required=False)
