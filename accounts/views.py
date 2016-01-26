a730d5fd3b4035fa2ac4a13b08d57337878d571b
Soenke Klinger <code@biver.de>
2016-01-26 21:29:37 +0100

    issue #326 (first version, no links to the shifts yet)

--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,12 @@ from django.views.generic.edit import UpdateView
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth import models
 
+from datetime import date, timedelta
+
 from volunteer_planner.utils import LoginRequiredMixin
+from scheduler.models import ShiftHelper
+from accounts.models import UserAccount
+
 
 
 @login_required()
@@ -75,3 +80,41 @@ class AccountDeleteView(LoginRequiredMixin, UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+@login_required()
+def shift_list_active(request):
+    """
+    Delivers the list of shifts, a user has signed up for today and the future.
+
+    :param request:
+    :return:
+    """
+    user = request.user
+    shifthelper = ShiftHelper.objects.filter(user_account=UserAccount.objects.get(user=user))
+    shifts_today = shifthelper.filter(shift__ending_time__day=date.today().day)
+    shifts_tomorrow = shifthelper.filter(shift__ending_time__day=date.today().day+1)
+    shifts_day_after_tomorrow = shifthelper.filter(shift__ending_time__day=date.today().day+2)
+    shifts_further_future = shifthelper.filter(shift__ending_time__gt=date.today() + timedelta(days=3))
+
+    return render(request, 'shift_list.html', {'user': user,
+                                               'shifts_today': shifts_today,
+                                               'shifts_tomorrow': shifts_tomorrow,
+                                               'shifts_day_after_tomorrow': shifts_day_after_tomorrow,
+                                               'shifts_further_future': shifts_further_future})
+
+@login_required()
+def shift_list_done(request):
+    """
+    Delivers the list of shifts, a user has signed up in the past (starting from yesterday).
+
+    :param request:
+    :return:
+    """
+    user = request.user
+    shifthelper = ShiftHelper.objects.filter(user_account=UserAccount.objects.get(user=user))
+    shifts_past =shifthelper.filter(shift__ending_time__lt=date.today())
+
+    return render(request, 'shift_list_done.html', {'user': user,
+                                                    'shifts_past': shifts_past})
+
+
