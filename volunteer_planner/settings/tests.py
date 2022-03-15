c6909801707744ddade41389cdbbc047e963130a
Christoph Meissner <post@christophmeissner.de>
2022-03-15 10:39:24 +0100

    Fix test configuration

--- volunteer_planner/settings/tests.py
+++ volunteer_planner/settings/tests.py
@@ -1,22 +1,10 @@
-from .base import *
+from .local import *
 from datetime import timedelta
-
-# Settings for running our tests
 
 DEBUG = False
 
 # Needed for letting Selenium access our server.
 ALLOWED_HOSTS = ['localhost']
 
-# TODO: Should run against MySQL
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        # Intentionally out of repository so that git clean doesn't delete the file.
-        'NAME': 'db.sqlite3',
-    }
-}
-
 SECRET_KEY = 'Kitten like fish'
-
 DEFAULT_SHIFT_CONFLICT_GRACE = timedelta(hours=1)
