34608c6f6cb86784d2343e822f0112fd7d1f7226
Christoph Meissner <post@christophmeissner.de>
2022-03-16 10:08:22 +0100

    Remove shiftmailer app

--- shiftmailer/admin.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-from django.contrib import admin
-from .models import Mailer
-from organizations.admin import MembershipFilteredAdmin
-
-
-@admin.register(Mailer)
-class MailerAdmin(MembershipFilteredAdmin):
-    pass
