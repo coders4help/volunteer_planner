aeb62dc13a0b46d84fd9d7268171a0f9b18d4db5
Christoph Meißner <github@christophmeissner.de>
2022-03-21 16:28:33 +0100

    Update makemessages.sh
    Co-authored-by: pitpalme <pitpalme@users.noreply.github.com>

--- makemessages.sh
+++ makemessages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cmd="python manage.py makemessages --all --no-wrap --no-location --no-obsolete"
+cmd="python3 manage.py makemessages --all --no-wrap --no-location --no-obsolete"
 echo $cmd
 ${cmd}
