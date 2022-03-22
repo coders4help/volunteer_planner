cde0ec7399c5bc01c630e6a19de9f1e5bc4ca32d
Christoph Meissner <post@christophmeissner.de>
2022-03-22 10:48:00 +0100

    Improve makemessages.sh
    You can now specify a language: arguments to `makemessages.sh` are now passed to `makemessages`. When no language is specified, `makemessages` will update all languages.
    
    Also, after `makemessages` is run, the diff is checked for non-trivial changes (aka. is any other change other than `POT-Creation-Date` present?) and a non-zero exit code is returned. This is used in the GitHub workflow to check if any messages are are missing for any translation.

--- makemessages.sh
+++ makemessages.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
-cmd="./manage.py makemessages --all --no-wrap --no-location --no-obsolete"
+
+cmd="./manage.py makemessages --no-wrap --no-location --no-obsolete $@"
 echo "${cmd}"
 ${cmd}
+echo "Done."
+echo
+echo "Checking for changes in .po files..."
+git diff --ignore-matching-lines=POT-Creation-Date --exit-code -- '***.po'
+
+diff_result=$?
+test $diff_result -eq 0 && echo "No changed messages found."
+echo
+exit $diff_result
