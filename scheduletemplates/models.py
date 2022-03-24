64e85876984561a21839af22f317ee64e461655f
Christoph Meissner <post@christophmeissner.de>
2022-03-24 20:34:48 +0100

    fixup pluralized translation

--- scheduletemplates/models.py
+++ scheduletemplates/models.py
@@ -1,9 +1,9 @@
 # coding: utf-8
-from datetime import timedelta, datetime, time
+from datetime import datetime, time, timedelta
 
 from django.db import models
 from django.templatetags.l10n import localize
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _, ngettext_lazy
 
 from . import managers
 
@@ -80,10 +80,10 @@ class ShiftTemplate(models.Model):
     @property
     def localized_display_ending_time(self):
         days = self.days if self.ending_time > time.min else 0
-        days_fmt = ungettext_lazy(u'the next day',
-                                  u'after {number_of_days} days', days)
-        days_str = days_fmt.format(
-            number_of_days=days) if days else u''
+        days_fmt = ngettext_lazy(u'the next day',
+                                 u'after {number_of_days} days',
+                                 days)
+        days_str = days_fmt.format(number_of_days=days) if days else u''
         return u'{time} {days}'.format(time=localize(self.ending_time),
                                        days=days_str).strip()
 
