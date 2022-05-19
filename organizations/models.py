coders4help/volunteer_planner

commit f3c6a5cf7138f51a5f544e21f882786afb85a795

Fix C417: generator expression instead of map and lambda

organizations/models.py
@@ -205,9 +205,7 @@ class Facility(models.Model):
 
     @property
     def address_line(self):
-        return ", ".join(
-            filter(None, map(lambda s: s.strip(), self.address.splitlines()))
-        )
+        return ", ".join((s.strip() for s in self.address.splitlines()))
 
     def __unicode__(self):
         return f"{self.name}"

commit 62f6b33070b6390b55f189844270c4c8378955e7

Fix f3c6a5c
Commit f3c6a5c introduced regression, not filtering out empty lines.

organizations/models.py
@@ -205,7 +205,7 @@ class Facility(models.Model):
 
     @property
     def address_line(self):
-        return ", ".join((s.strip() for s in self.address.splitlines()))
+        return ", ".join(filter(None, (s.strip() for s in self.address.splitlines())))
 
     def __unicode__(self):
         return f"{self.name}"